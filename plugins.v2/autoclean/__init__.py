bbd8595189450c69124409cfc6ad347823b30104
Griselda-droid <136452331+Griselda-droid@users.noreply.github.com>
2025-05-13 02:08:11 +0800

    Update __init__.py

--- plugins.v2/autoclean/__init__.py
+++ plugins.v2/autoclean/__init__.py
@@ -28,7 +28,7 @@ class AutoClean(_PluginBase):
     # 插件图标
     plugin_icon = "clean.png"
     # 插件版本
-    plugin_version = "2.1.1"
+    plugin_version = "2.1.2"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
